2a891f83e1ea00cda395c8c34f956d00b8703228
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-14 14:50:51 +0200

    add script
    for checking mail input in mailing-list and made result visible

---
+++ es-mail/js/script.js
@@ -0,0 +1,21 @@
+const mailList = ["alessia.franchi@gmail.com", "rossi@libero.it", "ilaria.verdi@tiscali.com"];
+
+const btnInput = document.getElementById("insert-btn");
+let output = document.getElementById("result");
+
+btnInput.addEventListener("click", function() {
+  const userMail = prompt("Inserisci qui il tuo indirizzo mail")
+  let isUserMail = false;
+
+  for (let i = 0; i < mailList.length; i++) {
+    if (mailList[i] === userMail) {
+      isUserMail = true;
+    }
+  }
+    if (isUserMail) {
+    output.innerHTML = ("Sei già iscritto alla newsletter");
+  }  else {
+    output.innerHTML = ("Non sei ancora iscritto alla newsletter, puoi procedere all'iscrizione");
+  }
+
+})
