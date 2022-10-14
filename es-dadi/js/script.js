c6243b1e6d6b9e1b8ce8f848a61b7bb3f097f4ea
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-14 17:04:07 +0200

    add style and emoji
    add smile emoji if user wins, sad emoji if pc wins, strenght emoji for
    tie

--- es-dadi/js/script.js
+++ es-dadi/js/script.js
@@ -1,19 +1,27 @@
 const diceBtn = document.getElementById("dice-btn");
 const pcOutput = document.getElementById("pc-output");
 const userOutput = document.getElementById("user-output");
-const result = document.getElementById("result")
+const sad = document.getElementById("sad");
+const smile = document.getElementById("smile");
+const result = document.getElementById("result");
 
 diceBtn.addEventListener("click", function(){
-  const pcNumber = Math.ceil(Math.random()*6);
-  const userNumber = Math.ceil(Math.random()*6);
+  const pcNumber = Math.ceil(Math.random() * 6);
+  const userNumber = Math.ceil(Math.random() * 6);
   pcOutput.innerHTML = "Il computer ha tirato: " + pcNumber;
   userOutput.innerHTML = "Tu hai tirato: " + userNumber;
+  smile.className = ("hide");
+  sad.className = ("hide");
+  strenght.className = ("hide");
 
   if (userNumber > pcNumber) {
     result.innerHTML = "Bravo! Hai vinto!"
+    smile.className = ("visible");
   } else if (userNumber === pcNumber) {
-    result.innerHTML = "Pareggio, non ha vinto nessuno! Riprova"
+    result.innerHTML = "Pareggio, non ha vinto nessuno! Riprova!"
+    strenght.className = ("visible");
   } else {
-    result.innerHTML = "Ha perso! Ritenta, sarai più fortunato!"
+    result.innerHTML = "Hai perso! Ritenta, sarai più fortunato!"
+    sad.className = ("visible");
   }
 })
